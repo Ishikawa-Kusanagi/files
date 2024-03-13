89a02f6ffcabe47ffd819705caac50cfdf8e3ccc
Ishikawa-Kusanagi <159976425+Ishikawa-Kusanagi@users.noreply.github.com>
2024-03-13 09:38:32 +0500

    Update text.py

--- text.py
+++ text.py
@@ -1,52 +1,32 @@
-def create_cook_book(file_path):
-    cook_book = {}
-
-    with open(file_path, 'r', encoding= 'utf-8') as f:
-        while True:
-            dish_name = f.readline().strip()
+def read_cook_book(file_name):
+    with open(file_name, 'r', encoding= 'utf-8') as f:
+        cook_book = {}
+        for line in f:
+            dish_name = line.strip()
             if not dish_name:
                 break
-
-            ingredients_count = int(f.readline().strip()) # убраить ошибку
-            ingredients = []
-
-            for _ in range(ingredients_count):
-                ingredient_info = f.readline().strip().split(' | ')
-                ingredient_name = ingredient_info[0]
-                quantity = int(ingredient_info[1])
-                measure = ingredient_info[2]
-
-                ingredient = {
-                    'ingredient_name': ingredient_name,
-                    'quantity': quantity,
-                    'measure': measure
-                }
-                ingredients.append(ingredient)
-
-            cook_book[dish_name] = ingredients
-
+            cook_book[dish_name] = []
+            num_ingredient = int(f.readline().strip())
+            for i in range(num_ingredient):
+                ingredient_name, quantity, measure = f.readline().split(' | ')
+                cook_book[dish_name].append({
+                    'ingredient_name' : ingredient_name,
+                    'quantity' : int(quantity),
+                    'measure' : measure.strip()
+                })
     return cook_book
-
-
-file_name = 'text.txt'
-cook_book = create_cook_book(file_name)
-# print(cook_book)
+print(read_cook_book('text.txt'))
 def get_shop_list_by_dishes(dishes, person_count):
+    cook_book = read_cook_book('text.txt')
     shop_list = {}
-
     for dish in dishes:
-        if dish in cook_book:
-            for ingredient in cook_book[dish]:
-                ingredient_name = ingredient['ingredient_name']
-                quantity = ingredient['quantity'] * person_count
-                measure = ingredient['measure']
-
-                if ingredient_name not in shop_list:
-                    shop_list[ingredient_name] = {'quantity': quantity, 'measure': measure}
-                else:
-                    shop_list[ingredient_name]['quantity'] += quantity
-
+        for ingredient in cook_book[dish]:
+            ingredient_name = ingredient['ingredient_name']
+            quantity = ingredient['quantity'] * person_count
+            measure = ingredient['measure']
+            if ingredient_name in shop_list:
+                shop_list[ingredient_name]['quantity'] += quantity
+            else:
+                shop_list[ingredient_name] = {'measure': measure, 'quantity': quantity}
     return shop_list
-
-
-print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+print(get_shop_list_by_dishes('Омлет', 2))
